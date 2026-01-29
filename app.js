646a78b5ff28f5b0de28dee6a84c6d83502c8e86
gianninimatteo <matteogiannini294@gmail.com>
2026-01-29 22:16:39 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,379 @@
+const Home = {
+    template: `
+        <div class="home-content">
+            <main>
+                <h2>Cosa è l'Augmented Reality on Web?</h2>
+                <article>
+                    <p>La realtà aumentata sul web è una tecnologia che consente agli utenti di vivere esperienze di realtà aumentata direttamente dal proprio browser web, senza la necessità di scaricare app dedicate. 
+                    Ciò significa che è possibile visualizzare oggetti 3d virtuali sovrapposti al mondo reale semplicemente scansionando un codice QR o cliccando su un link speciale. Questo apre una serie di possibilità 
+                    interessanti per le aziende e i privati, che possono utilizzare la WebAR per una varietà di scopi tra cui:</p>
+                    <ul>
+                        <li>
+                            <p>MARKETING E PUBBLICITÀ: le aziende possono utilizzare la WebAR per creare esperienze di marketing immersive 
+                            che consentono ai clienti di interagire con i prodotti in modo più realistico. Ad esempio, un’azienda di mobili 
+                            potrebbe consentire ai clienti di visualizzare un nuovo divano nel proprio soggiorno prima di acquistarlo.</p>
+                        </li>
+                        <li>
+                            <p>FORMAZIONE ED EDUCAZIONE: la WebAR può essere utilizzata per creare esperienze di apprendimento più 
+                            coinvolgenti e interattive. Ad esempio, un museo potrebbe utilizzare la WebAR per consentire 
+                            ai visitatori di conoscere in dettaglio i reperti esposti.</p>
+                        </li>
+                        <li>
+                            <p>INTRATTENIMENTO: la WebAR può essere utilizzata per creare giochi e altre esperienze di intrattenimento immersive. 
+                            Ad esempio, un videogioco potrebbe utilizzare la WebAR per consentire ai giocatori di esplorare il mondo di gioco in modo più realistico.</p>
+                        </li>
+                    </ul>
+                </article>
+            </main>
+            <aside>
+                <h2>Realtà Aumentata (AR) VS Realtà Virtuale (VR)</h2>
+                <article>
+                <ul>
+                    <li>
+                        <p>REALTA' AUMENTATA (AR): Sovrappone elementi digitali al mondo reale. In questo modo, puoi vedere il mondo il mondo che ti circonda e allo stesso tempo avere informazioni digitali sovrapposte ad esso. Ad esempio, puoi usare un'app AR per vedere le indicazioni stradali sovrapposte alla tua vista della strada, oppure puoi vedere modelli 3D di mobili posizionati nella tua stanza.</p>
+                        </li>
+                    <li>
+                        <p>REALTA' VIRTUALE (VR): Crea un ambiente totalmente simulato che sostituisce il mondo reale. quando indossi un visore VR, sei completamento immerso in questo mondo virtuale e non puoi vedere il mondo reale che ti circonda. Questo può essere usato per creare esperienze molto immersive, come giocare a videogiochi o viaggiare in posti virtuali.</p>
+                    </li>
+                        </ul>
+                </article>
+            </aside>
+        </div>
+    `
+};
+
+
+
+
+const Informazioni = {
+    template: `
+         <div class="home-content">
+         <main>
+
+              <h2>Le Origini della WebAR</h2>
+            <section>
+                <ul>
+                    <li>Anni 90: Nasce il concetto di realtà aumentata con i primi prototipi per uso militare</li>
+                    <li>2000: Tom Caudelle conia il termine "AR"</li>
+                    <li>2008: Lancio del web browser "Wikitude"</li>
+                    <li>2009: Google presenta "Google Glass", un visore AR che però non ha avuto successo</li>
+                </ul>
+            </section>
+        
+              <h2>Oggi</h2>
+            <section>
+                <ul>
+                    <li>La WebAR è una tecnologia in rapida crescita con un enorme potenziale per diversi settori. Viene utilizzata per una vasta gamma di applicazioni, tra cui:</li>
+                    <li>Pubblicità</li>
+                    <li>E-commerce</li>
+                    <li>Istruzione</li>
+                    <li>Turismo</li>
+                    <li>Giochi</li>
+                    <li>Non richiede installazione di App, accessibile tramite un semplice link web</li>
+                </ul>
+            </section>
+        
+              <h2>Sviluppo e Diffusione</h2>
+            <section>
+                <ul>
+                    <li>2012: Nascono le prime piattaforme WebAR come Layar e Junajo</li>
+                    <li>2015: Apple introduce il supporto AR nativo per IOS con ARKit</li>
+                    <li>2017: Google lancia ARCore per Android</li>
+                    <li>2018: Nasce la WebAR Consortium per promuovere standard aperti per il WebAR</li>
+                </ul>
+            </section>
+
+               <h2>Vantaggi della WebAR</h2>
+            <section>
+                <p>La WebAR offre una serie di vantaggi rispetto alle tradizionali esperienze AR che richiedono il download di app dedicate. Questi vantaggi includono:</p>
+                <ul>
+                    <li>MAGGIORE ACCESSIBILITÀ: la WebAR è accessibile a chiunque disponga di uno smartphone e di un browser web moderno, non è necessario scaricare app dedicate, il che la rende molto più accessibile rispetto alle tradizionali esperienze AR.</li>
+                    <li>MINORE COMPLESSITÀ: la WebAR è più semplice da sviluppare e implementare rispetto alle tradizionali esperienze AR. Ciò significa che è più economica e accessibile alle aziende e ai privati.</li>
+                    <li>MAGGIORE SCALABILITÀ: le esperienze WebAR possono essere facilmente scalate per raggiungere un vasto pubblico. Ciò le rende ideali per le campagne di marketing e pubblicità su larga scala.</li>
+                    <li>CONDIVISIONE: i contenuti Web AR possono essere facilmente condivisi con altri tramite link o codici QR.</li>
+                    <li>VERSATILITA' : la Web AR può essere utilizzata per una vasta gamma di applicazioni, come marketing, istruzione, formazione, giochi e altro ancora.</li>
+                </ul>
+            </section>
+        </main>
+
+        <aside id="aside2">
+            <h2>Come funziona la WebAR?</h2>
+            <article>
+                <ul>
+                    <li>
+                         <p>La WebAR funziona sfruttando le funzionalità di realtà aumentata integrate nei browser web moderni. Quando un utente scansiona un codice QR o clicca un link speciale, il browser web carica una pagina web che include istruzioni su come visualizzare l’oggetto 3D virtuale. La fotocamera del dispositivo dell’utente viene quindi utilizzata per tracciare il mondo reale e posizionare l’oggetto 3D virtuale nello spazio corretto.</p>
+                    </li>
+                </ul>
+            </article>
+           
+            <h2>Il Futuro della WebAR</h2>
+             <article>
+                <ul>
+                    <li>
+                         <p>La WebAR è una tecnologia relativamente nuova, ma ha già il potenziale per rivoluzionare il modo in cui interagiamo con il mondo digitale. Con il continuo sviluppo della tecnologia AR e web, possiamo aspettarci di vedere ancora più esperienze WebAR innovative e coinvolgenti nei prossimi anni.</p>
+                    </li>
+                </ul>
+            </article>
+
+            <h2>Come viene usata la WebAR:</h2>
+            <article>
+                <ul>
+                    <li>Visualizzare prodotti in 3D: i clienti possono visualizzare prodotti come mobili o vestiti in scala reale nelle loro case prima di acquistarli.</li>
+                    <li>Esplorare luoghi: è possibile visitare musei o siti storici virtuali e conoscere la loro storia attraverso contenuti AR.</li>
+                    <li>Ottenere istruzioni: la Web AR può essere utilizzata per fornire indicazioni stradali o istruzioni di montaggio per prodotti.</li>
+                    <li>Giocare: esistono giochi AR che permettono ai giocatori di interagire con il mondo reale in modi nuovi e coinvolgenti.</li>
+                </ul>
+            </article>
+        
+        </aside>
+    </div>
+
+        <main id="main2">
+        <section>
+                <h2>Esempio di Realtà aumentata</h2>
+                <section>
+                    <p>Si può sperimentare la realtà aumentata direttamente sul tuo telefono senza installare alcuna applicazione, grazie alla libreria AR.js che permette la creazione di esperienze di realtà aumentata sul web.</p>
+                    <p>Ecco il codice completo:</p>
+                    <pre>
+                        <code>
+                            &lt;script src="https://aframe.io/releases/0.8.0/aframe.min.js"&gt;&lt;/script&gt;
+                            &lt;script src="https://cdn.rawgit.com/jeromeetienne/AR.js/1.6.0/aframe/build/aframe-ar.js"&gt;&lt;/script&gt;
+                            &lt;body style='margin : 0px; overflow: hidden;'&gt;
+                                &lt;a-scene embedded arjs='sourceType: webcam;'&gt;
+                                    &lt;a-box position='0 0.5 0' material='opacity: 0.5;'&gt;&lt;/a-box&gt;
+                                    &lt;a-marker-camera preset='hiro'&gt;&lt;/a-marker-camera&gt;
+                                &lt;/a-scene&gt;
+                            &lt;/body&gt;
+                        </code>
+                    </pre>
+                    <p>Analizziamo il codice:</p>
+                    <h3>Aggiungere le biblioteche</h3>
+                    <pre>
+                        <code>
+                            &lt;script src="https://aframe.io/releases/0.8.0/aframe.min.js"&gt;&lt;/script&gt;
+                            &lt;script src="https://cdn.rawgit.com/jeromeetienne/AR.js/1.6.0/aframe/build/aframe-ar.js"&gt;&lt;/script&gt;
+                        </code>
+                    </pre>
+                    <p> Queste librerie sono per la creazione di esperienze di realtà aumentata (AR) basate sul web.</p>
+                    <h3>Definire il corpo</h3>
+                    <pre>
+                        <code>
+                            &lt;body style='margin : 0px; overflow: hidden;'&gt;
+                            &lt;a-scene embedded arjs='sourceType: webcam;'&gt;
+                                &lt;a-box position='0 0.5 0' material='opacity: 0.5;'&gt;&lt;/a-box&gt;
+                                &lt;a-marker-camera preset='hiro'&gt;&lt;/a-marker-camera&gt;
+                            &lt;/a-scene&gt;
+                        &lt;/body&gt;
+                        </code>
+                    </pre>
+                
+                    <h3>Creazione di una scena 3D</h3>
+                    <pre>
+                        <code>
+                            &lt;a-scene embedded arjs='sourceType: webcam;'&gt;
+                                    &lt;a-box position='0 0.5 0' material='opacity: 0.5;'&gt;&lt;/a-box&gt;
+                                    &lt;a-marker-camera preset='hiro'&gt;&lt;/a-marker-camera&gt;
+                                &lt;/a-scene&gt;
+                        </code>
+                    </pre>
+                    <p>Questo codice crea una scena di realtà aumentata che mostra un cubo semitrasparente al centro della vista della fotocamera quando viene rilevato il marker "hiro".</p>
+                    
+                    <h3>Aggiunta di contenuti speciali</h3>
+                    <pre>
+                        <code>
+                            &lt;a-box position='0 0.5 0' material='opacity: 0.5;'&gt;&lt;/a-box&gt;
+                        </code>
+                    </pre>
+                    <p>Questa riga di codice crea un cubo semitrasparente posizionato al centro della scena di A-Frame, a mezza unità sopra l'asse Y.</p>
+                
+                    <h3>Aggiunta della fotocamera AR</h3>
+                    <pre>
+                        <code>
+                            &lt;a-marker-camera preset='hiro'&gt;&lt;/a-marker-camera&gt;
+                        </code>
+                    </pre>
+                
+                    <p>Per provare l'effetto della realtà aumentata vai sul sito di <a href="https://codepen.io/jeromeetienne/pen/mRqqzb">CODEPEN. </a>Inquadra un'immagine di Hiro o stampala per vedere il risultato.</p>
+                </section>
+            </section>
+        </main>
+            
+    `,
+};
+
+const Dati = {
+    template: `
+        <div class="container">
+            <h2>Tecnologie</h2>
+            <table v-if="invenzioni">
+                <tr>
+                    <th>Nome Tecnologia</th>
+                    <th>Inventore</th>
+                    <th>Anno di Invenzione</th>
+                    <th>Impatto Principale</th>
+                </tr>
+                <tr v-for="invenzione in invenzioni" :key="invenzione.id">
+                     <td>{{ invenzione.NomeTecnologia }}</td>
+                    <td>{{ invenzione.Inventore }}</td>
+                    <td>{{ invenzione.AnnoInvenzione }}</td>
+                    <td>{{ invenzione.Impatto }}</td>
+                </tr>
+            </table>
+            <div v-else>
+                Caricamento in corso...
+            </div>
+        </div>
+    `,
+    data() {
+        return {
+            invenzioni: null
+        };
+    },
+    methods: {
+        getData() {
+            axios.get('dati.json')
+                .then(response => {
+                    this.invenzioni = response.data;
+                })
+                .catch(error => {
+                    console.error('Errore nel caricamento dei dati:', error);
+                });
+        }
+    },
+    mounted() {
+        this.getData();
+    }
+};
+
+
+const InserimentoDati = {
+    template: `
+        <div class="container">
+          <h2>Lista Tecnologie</h2>
+            <table v-if="invenzioni">
+                <tr>
+                    <th>Nome Tecnologia</th>
+                    <th>Inventore</th>
+                    <th>Anno di Invenzione</th>
+                    <th>Impatto Principale</th>
+                    <th>Azioni</th>
+                </tr>
+                <tr v-for="invenzione in invenzioni" :key="invenzione.id">
+                    <td>{{ invenzione.NomeTecnologia }}</td>
+                    <td>{{ invenzione.Inventore }}</td>
+                    <td>{{ invenzione.AnnoInvenzione }}</td>
+                    <td>{{ invenzione.Impatto }}</td>
+                    <td>
+                        <button @click="updateData(invenzione.id)">Aggiorna</button>
+                        <button @click="deleteData(invenzione.id)">Elimina</button>
+                    </td>
+                </tr>
+            </table>
+            <div v-else>
+                Caricamento in corso...
+            </div>
+            <button @click="createData">Crea nuova Tecnologia</button>
+        </div>
+    `,
+    data() {
+        return {
+            invenzioni: null
+        };
+    },
+    methods: {
+        getData() {
+            axios.get('dati.json')
+                .then(response => {
+                    this.invenzioni = response.data;
+                })
+                .catch(error => {
+                    console.error('Errore nel caricamento dei dati:', error);
+                });
+        },
+        
+        //MODIFICA DATI
+        updateData(id) { //l'ID ServE a specificare la tecnologia specifica che vogliamo andare a modificare
+            const item = this.invenzioni.find(invenzione => invenzione.id === id); //Trova la tecnologia specifica con l'id
+            if (!item) return; // se Non trova la tecnologia  esce dalla funzione
+
+            const nuovaTecnologia = prompt(`Inserisci il nome della tecnologia:`, item.NomeTecnologia);
+            const nuovoInventore = prompt(`Inserisci l'inventore:`, item.Inventore);
+            const nuovoAnno = prompt(`Inserisci l'anno:`, item.AnnoInvenzione);
+            const nuovoImpatto = prompt(`Inserisci l'impatto principale:`, item.Impatto);
+
+            //caso in cui si lascia uno o più campi Vuoti.
+            if (!nuovaTecnologia || !nuovoInventore || !nuovoAnno || !nuovoImpatto) {
+                alert("Errore: inserire tutti i campi");
+                return;
+            }
+
+            //Aggiornamento dei nuovi Valori.
+            item.NomeTecnologia = nuovaTecnologia;
+            item.Inventore = nuovoInventore;
+            item.AnnoInvenzione = nuovoAnno;
+            item.Impatto = nuovoImpatto;
+       
+        },
+
+        //CANCELLAZIONE DATI
+        deleteData(id) {
+            if (!confirm(`Confermi l'eliminazione dell'elemento?`)) return; //cancellazione elemento
+
+            const index = this.invenzioni.findIndex(item => item.id === id); //trova l'Indice dell'elemento. findIndex trova l'elemento specifico.
+            if (index !== -1) { //se l'elemento viene trovAto (diverso da -1) viene rimosso
+                this.invenzioni.splice(index, 1); //splice Rimuove l'elemento all'indice specificato da index.
+            } else {
+                alert("Elemento non trovato."); //altrimenti se non viene trovato da messaggio di errore
+            }
+        },
+
+        //CREAZIONE DATI
+        createData() {
+            const NomeTecnologia = prompt("Inserisci il nome della tecnologia:");
+            const Inventore = prompt("Inserisci il nome dell'inventore:");
+            const AnnoInvenzione = prompt("Inserisci l'anno dell'invenzione:");
+            const Impatto = prompt("Inserisci l'impatto principale:");
+
+            if (!NomeTecnologia || !Inventore || !AnnoInvenzione || !Impatto) { //caso in cui si lascia uno o più campi Vuoti.
+                alert("Errore: inserire tutti i campi");
+                return;
+            }
+
+            //crea un nuovo elemento
+            const newItem = {
+                id: Date.now(), // Genera un ID unico
+                NomeTecnologia,
+                Inventore,
+                AnnoInvenzione,
+                Impatto
+            };
+            this.invenzioni.push(newItem); //aggiunge il nuovo Elemento alla lista.
+
+        }
+    },
+    mounted() {
+        this.getData();
+    }
+};
+
+
+
+
+//associamo i componenti alle rotte
+const routes = [
+    { path: '/', component: Home },
+    { path: '/informazioni', component: Informazioni },
+    { path: '/dati', component: Dati },
+    { path: '/inserimentoDati', component: InserimentoDati }
+];
+
+//instanziare vuerouter
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHashHistory(),
+    routes
+});
+
+//passare VueRouter a vueapp
+const app = Vue.createApp({});
+app.use(router);
+app.mount('#app');
